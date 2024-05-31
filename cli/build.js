567de3620a0163026354096343a220c19c65b41a
MI Sajid <misajidsa@gmail.com>
2024-06-01 01:44:59 +0600

    feat: add confirmation after building the app

--- cli/build.js
+++ cli/build.js
@@ -12,4 +12,6 @@ console.log("Compiling TypeScript");
 execSync("npx tsc", {
     stdio: "inherit",
 });
+const code = fs.readFileSync("./build/index.js");
+fs.writeFileSync("./build/index.js", `process.env.NODE_ENV = "production"\n` + code);
 console.log(chalk.greenBright(`Application has been built for production \nuse ${chalk.yellowBright("npm run start")}`));
