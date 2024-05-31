800bb6b245d77d555e783ee8b3a86b0f0de20b8e
MI Sajid <misajidsa@gmail.com>
2024-06-01 01:46:40 +0600

    feat: add socket connections for chat app

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,73 @@
-import { Server } from "socket.io";
-import express from "express";
-import http from "http";
 import chalk from "chalk";
-const app = express();
-const server = http.createServer(app);
-app.use(express.static("./build/dist"));
+import { IChat } from "./types/types";
+import { v4 } from "uuid";
+import { nameValidator } from "./validations/name.validator";
+import path from "path";
+import { app, io, server } from "./server";
+import { removeRoomIfNoActivity } from "./helpers/removeRoomIfNoActivity";
+import { rooms } from "./rooms";
+import { getRoomFromSocketID } from "./helpers/getRoomFromSocketID";
+import { removeUserFromRooms } from "./helpers/removeUserFromRooms";
+import { z } from "zod";
 
-app.get("/", (_, res) => {
-    res.send("index");
+app.get("*", (_, res) => {
+    if (process.env.NODE_ENV !== "production") return res.send("This is not a production build <br><br> run: <br><br> <code>npm run build</code> <br> <code>npm run start</code> <br><br> to serve a production build");
+    res.sendFile(path.join(process.cwd(), "build", "dist", "index.html"));
 });
 
-const io = new Server(server);
+io.on("connection", (socket) => {
+    socket.emit("rooms", rooms);
+    socket.emit("connection", socket.id);
 
-io.on("connection", () => {
-    console.log("connected");
+    socket.on("create", (name: string) => {
+        const id = v4();
+        rooms.push({
+            name: name,
+            id: id,
+            sockets: [],
+            lastActivity: Date.now(),
+            chats: [],
+        });
+        const interval = setInterval(() => removeRoomIfNoActivity(id, interval), 30000);
+        io.emit("rooms", rooms);
+        socket.emit("redirectToHome");
+    });
+
+    socket.on("joinRoom", async (id: string, name: string) => {
+        if (nameValidator.safeParse(name).error) return socket.emit("redirectToHome");
+        const room = rooms.find((e) => e.id === id);
+        if (!room) return socket.emit("redirectToHome");
+        removeUserFromRooms(socket.id, socket);
+        await socket.join(id);
+        room.lastActivity = Date.now();
+        room.sockets.push(socket.id);
+        io.emit("rooms", rooms);
+        room.chats.push({ type: "system", message: `${name} joined the room` });
+        io.to(id).emit("roomMessage", room.chats);
+    });
+
+    socket.on("roomMessage", ({ message }: { [key: string]: string }) => {
+        if (!message) return;
+        const { room } = getRoomFromSocketID(socket.id);
+        if (!room) return;
+        const validate = z.string().max(2000).safeParse(message);
+        if (validate.error) return socket.emit("notification", "warning", validate.error.formErrors.formErrors[0]);
+        room.chats.push({
+            type: "user",
+            name: socket.handshake.auth.name,
+            socket_id: socket.id,
+            message,
+        } as IChat);
+        socket.emit("setMessageInput", "");
+        io.to(room.id).emit("roomMessage", room.chats);
+    });
+
+    socket.on("disconnect", () => {
+        removeUserFromRooms(socket.id, socket);
+        io.emit("rooms", rooms);
+    });
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log(chalk.green("Backend is running on", chalk.blueBright("http://localhost:3000")));
 });
