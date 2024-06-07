d6a7391d6b2204db6cd44d1b4157347a13569c0d
MI Sajid <misajidsa@gmail.com>
2024-06-07 23:57:34 +0600

    fix: responsiveness on mobile device

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     const navigate = useNavigate();
 
     return (
-        <div className="h-full w-full p-5 flex flex-wrap bg-slate-100 gap-5">
+        <div className="h-full w-full p-5 flex flex-wrap content-start bg-slate-100 gap-5">
             <div className="fixed top-2 right-2 flex gap-3 items-center">
                 <NewRoomDialog />
                 <NameDialog />
@@ -19,7 +19,7 @@ export default function Home() {
                 <div className="w-full h-full flex items-center justify-center text-black/50 font-medium">No Rooms</div>
             ) : (
                 rooms.map((e, i) => (
-                    <div key={i} onClick={() => navigate(e.id)} className="size-40 cursor-pointer p-2 rounded-md border flex flex-col justify-between bg-white shadow-sm">
+                    <div key={i} onClick={() => navigate(e.id)} className="size-32 sm:size-40 cursor-pointer p-2 rounded-md border flex flex-col justify-between bg-white shadow-sm">
                         <h2 className="font-semibold truncate">{e.name}</h2>
                         <div className="w-full justify-end gap-2 flex items-center">
                             <span className="text-sm">{e.sockets.length}</span> <IoMdPeople />
